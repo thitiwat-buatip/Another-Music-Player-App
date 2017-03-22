94823b0a28370f93d0cbb6a0051be119da9be304
thitiwat-buatip <subin2538@gmail.com>
2017-03-22 14:36:49 +0700

    Initial Commit

---
+++ Another Music Player App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Another Music Player App
+//
+//  Created by thitiwat on 22/3/60.
+//  Copyright © พ.ศ. 2560 thitiwat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
